e7005c2393319ec6603701a34328d7ed45d1b13b
Enzo Josue Nina Aragon <U19221435@utp.edu.pe>
2021-10-04 21:55:04 -0500

    Final

--- js/fullpage.js
+++ js/fullpage.js
@@ -6,11 +6,10 @@ var seccionesPagina = new fullpage('#fullpage',{
     scrollingSpeed: 700, // Velocidad del scroll. Valores: en milisegundos.
     css3: true, // Si usara CSS3 o javascript.
     easingcss3: 'ease-out', // Curva de velocidad del efecto.
-    loopBottom: true, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.
-    
+    loopBottom: true, // Regresa a la primera seccion siempre y cuando se ya haya llegado a la ultima sección y el ususario siga scrolleando.    
     navigation: true, // Muesta la barra de navegación.
     menu: '#menu', // Menu de navegación.
     anchors: ['infor','perfil','campo','malla'], // Anclas, las usamos para identificar cada seccion y poder acceder a ellas con el menu.
     navigationTooltips: ['Info','Perfil', 'Campo','Malla'], // Tooltips que mostrara por cada boton.
-    showActiveTooltip: false, // Mostrar tooltip activa.
+    showActiveTooltip: true, // Mostrar tooltip activa.    
 });
